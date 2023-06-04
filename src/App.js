88b1370695e31d3859095216a3a76d4227b51e83
LyricsON <lyricson0203@gmail.com>
2023-06-04 16:56:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TaskList from "./Components/TaskList";
+import "./App.css";
+import AddNewTask from "./Components/AddNewTask";
+export default class App extends Component {
+  state = {
+    todos: [
+      { id: Math.random(), action: "wake Up", isDone: true },
+      { id: Math.random(), action: "Go Out", isDone: true },
+      { id: Math.random(), action: "Buy Coffe", isDone: true },
+      { id: Math.random(), action: "read a book", isDone: true },
+    ],
+  };
+  // delete function
+  handleDelete = (id) =>
+    this.setState({ todos: this.state.todos.filter((el) => el.id !== id) });
+  // complete function
+  handleComplete = (ID) =>
+    this.setState({
+      todos: this.state.todos.map((el) =>
+        el.id === ID ? { ...el, isDone: !el.isDone } : el
+      ),
+    });
+  // add function
+  handleAdd = (newTask) =>
+    this.setState({ todos: [newTask, ...this.state.todos] });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <AddNewTask handleAddFunction={this.handleAdd} />
+        <TaskList
+          list={this.state.todos}
+          deleteFunction={this.handleDelete}
+          CompleteFunction={this.handleComplete}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
